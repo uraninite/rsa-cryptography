e4315d9761619ef5049a6c8cb0a209343e492b86
X-iv <72281205+X-iv@users.noreply.github.com>
2021-07-23 01:48:22 +0300

    Update hashbin.py

--- src/mod/hashbin.py
+++ src/mod/hashbin.py
@@ -2,12 +2,59 @@
 import hashlib
   
 # initializing string
-str = "TOKENAUTHNLL"
+str = "TOKENAUTHNLX"
   
 # encoding GeeksforGeeks using encode()
 # then sending to SHA256()
-result = hashlib.sha512(str.encode())
-  
+token = hashlib.sha512(str.encode())
+
+
+def modrandfunc(token, n):
+        token = token.digest().hex()
+ 
+        if (len(token) < n):
+                j = n - 128
+                
+                longByte = ""
+
+                while (len(longByte) <= n):
+                        longByte+=token
+                
+                #modrandfunc(token, )
+                
+                print(j, n)
+        
+
+
+                return longByte[:n]
+        elif (len(token) == n): 
+                return token
+        else:
+                return token[:n]
+
+print(modrandfunc(token, 500))
+
+print("\n")
+
 # printing the equivalent hexadecimal value.
-print("The binary equivalent of SHA256 is : ")
-print(result.digest())
+print("The binary equivalent of SHA256 is :")
+
+print("\n")
+
+
+
+
+print(token.digest())
+
+print("\n")
+
+print(token.digest().hex())
+
+print("\n")
+
+print(bytes.fromhex(token.digest().hex()))
+
+print("\n")
+
+print(token.digest().hex()[:2])
+
